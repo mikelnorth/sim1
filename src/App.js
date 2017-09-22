d851c73791c8514a7b7c2557d22d3278fa581bf7
Erin Schull <erinv.schull@Erins-MacBook-Pro.local>
2017-09-22 15:24:33 -0600

    home added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/home/Home.js';
+import router from './router.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          blarg <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      { router }
+      <Home />
     );
   }
 }
---
+++ src/component/home/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div className='Shelfie__parent_container'>
+                <div className='Shelfie_header' >
+                    <div className='Shelfie_header_content' >
+                        <img src= {Logo} className='Shelfie_header_logo' alt='logo' />
+                        <span className='Shelfie__header_span'>SHELFIE</span>
+
+                    </div>
+                </div>
+            <body className='Shelfie__child_container' >
+                <Link className='navLink' to= '/' > 
+                <span className='Shelf_A'>Shelf A</span>
+                </Link>
+                <Link className='navLink' to= '/'>
+                <span className='Shelf_B'>Shelf B</span>
+                </Link>
+                <Link className='navLink' to= '/'>
+                <span className='Shelf_C'>Shelf C</span>
+                </Link>
+                <Link className='navLink' to= '/'>
+                <span className='Shelf_D'>Shelf D</span>
+                </Link>
+            </body>
+
+            </div>
+        )
+    }
+}
+export default Home; 
